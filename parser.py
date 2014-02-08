13d061254d4cb3bab11c4ae83d3d3bb04a0c2448
jds8 <jds464@cornell.edu>
2014-02-08 16:37:06 -0500

    Updated Review class

--- parser.py
+++ parser.py
@@ -18,6 +18,23 @@ class Review:
         self.funny = funny
         self.date = date
 
+class Business:
+    def __int__(self, type, business_id, stars, review_count):
+        self.type = type
+        self.business_id = business_id
+        self.stars = stars
+        self.review_count = review_count
+
+class User:
+    def __init__(self, user_id, tag, name, review_count, average_stars, useful, cool, funny):
+        self.user_id = user_id
+        self.tag = tag
+        self.name = name
+        self.review_count = review_count
+        self.average_stars = average_stars
+        self.useful = useful
+        self.cool = cool
+        self.funny = funny
 
 def parse_reviews(filename):
 	matcher = re.compile(r'([01])|([01?])|(.+)|(.+)|(.+)|(.+)|(.+)|(.+)|(.+)|(.+)')
@@ -37,3 +54,4 @@ if __name__ == '__main__':
 	data = parse_data('../data/Train data')
 	for review in data:
 		print "type: %s review_id: %s tag: %s text %s user_id %s business_id %s stars %s useful %s cool %s funny %s date %s" % (review.type, review.review_id, review.tag, review.text, review.user_id, review.business_id, review.stars, review.useful, review.cool, review.funny, review.date)
+
